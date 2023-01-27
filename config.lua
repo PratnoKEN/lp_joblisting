add805b4b7d532df5c2880032f5976a559f35338
Ibnu Hanafi <62471250+PratnoKEN@users.noreply.github.com>
2023-01-27 20:46:38 +0700

    Update config.lua

--- config.lua
+++ config.lua
@@ -1,16 +1,15 @@
 Config              = {}
 
 Config.DrawDistance = 10.0
-Config.ZoneSize     = {x = 2.7, y = 2.7, z = 0.5}
+Config.ZoneSize     = {x = 0.3, y = 0.3, z = 0.1}
 Config.MarkerColor  = {r = 100, g = 200, b = 104}
-Config.MarkerType   = 27
-Config.Debug = ESX.GetConfig().EnableDebug
-Config.target = true
+Config.MarkerType   = 2
+Config.target = false
 
 Config.Locale       = 'en'
 
 Config.Zones = {
-	vector3(-265.08, -964.1, 30.3)
+	vector3(-1082.123047, -247.516479, 37.7554371)
 }
 
 Config.Blip = {
