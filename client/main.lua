8569e93347aa78c380a6ff2076ad24497b7e8425
Ibnu Hanafi <62471250+PratnoKEN@users.noreply.github.com>
2023-03-21 23:01:51 +0700

    Update main.lua

--- client/main.lua
+++ client/main.lua
@@ -1,5 +1,5 @@
 RegisterNetEvent('jobcenter:ShowJobListingMenu', function() 
-	ESX.TriggerServerCallback('lp_joblisting:getJobsList', function(jobs)
+	lib.callback('lp_joblisting:getJobsList', 0, function(jobs)
 		local elements = {}
 
 		for k,v in pairs(jobs) do
@@ -7,6 +7,7 @@ RegisterNetEvent('jobcenter:ShowJobListingMenu', function()
 				title   = k,
 				header = v.label,
 				event = 'jobcenter:list',
+				icon = Config.JobIcons[k],
 				args = {
 					job = k
 				}
@@ -32,61 +33,47 @@ RegisterNetEvent('jobcenter:list', function(data)
 	})
 end)
 
-if Config.target then
-	exports.ox_target:addBoxZone({
-		coords = vec3(-1082.123047, -247.516479, 37.7554371),
-		size = vec3(1, 1, 1),
-		rotation = 19.8425,
-		debug = true,
-		options = {
-			{
-				name = 'Jobcentertarget',
-				event = "jobcenter:ShowJobListingMenu",
-				icon = "fas fa-sign-in-alt",
-				label = "Job Center",
-			},
-		}
-	})
-else
-	CreateThread(function()
-		while true do
-			local Sleep = 1500
+JobCenterPoints = function()
+	for k,v in pairs(Config.Zones) do
+		local coords = v.pointcoords
+		local point = lib.points.new(coords, 2)
 
-			local coords = GetEntityCoords(cache.ped)
-
-			for i=1, #Config.Zones, 1 do
-				local distance = #(coords - Config.Zones[i])
-
-				if distance < Config.DrawDistance then
-					Sleep = 0
-					DrawMarker(Config.MarkerType, Config.Zones[i], 0.0, 0.0, 0.0, 0, 0.0, 0.0, Config.ZoneSize.x, Config.ZoneSize.y, Config.ZoneSize.z, Config.MarkerColor.r, Config.MarkerColor.g, Config.MarkerColor.b, 100, false, true, 2, false, false, false, false)
-				end
-
-				if distance < (Config.ZoneSize.x / 2) then
-					ESX.ShowHelpNotification(_U('access_job_center'))
-					if IsControlJustReleased(0, 38) then
-						TriggerEvent('jobcenter:ShowJobListingMenu')
-					end
-				end
-			end
-		Wait(Sleep)
+		function point:onEnter() 
+			lib.showTextUI('[E] - Acces Job Center')
 		end
-	end)
+
+		function point:onExit() 
+			lib.hideTextUI()
+		end
+
+		function point:nearby()
+			DrawMarker(2, self.coords.x, self.coords.y, self.coords.z, 0.0, 0.0,0.0, 0.0, 180.0, 0.0, 0.7, 0.7, 0.7, 225, 225, 211, 50, false,true, 2, nil, nil, false)
+			if self.currentDistance < 1 and IsControlJustReleased(0, 38) then
+				TriggerEvent('jobcenter:ShowJobListingMenu')
+			end
+		end
+		return point
+	end
 end
 
 -- Create blips
-CreateThread(function()
-	for i=1, #Config.Zones, 1 do
-		local blip = AddBlipForCoord(Config.Zones[i])
-
-		SetBlipSprite (blip, Config.Blip.Sprite)
-		SetBlipDisplay(blip, Config.Blip.Display)
-		SetBlipScale  (blip, Config.Blip.Scale)
-		SetBlipColour (blip, Config.Blip.Colour)
-		SetBlipAsShortRange(blip, Config.Blip.ShortRange)
-
+CreateBlips = function()
+	for k,v in pairs(Config.Zones) do
+		local blip = AddBlipForCoord(v.pointcoords.x,v.pointcoords.y,v.pointcoords.z)
+		SetBlipSprite(blip,407)
+		SetBlipColour(blip,27)
+		SetBlipAsShortRange(blip,true)
+		SetBlipScale(blip,0.6)
 		BeginTextCommandSetBlipName("STRING")
-		AddTextComponentSubstringPlayerName(_U('job_center'))
+		AddTextComponentString(_U('job_center'))
 		EndTextCommandSetBlipName(blip)
 	end
+end
+
+CreateThread(function()
+	while not ESX.IsPlayerLoaded() do
+        Wait(100)
+    end
+	JobCenterPoints()
+	CreateBlips()
 end)
