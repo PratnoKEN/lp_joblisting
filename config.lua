95d1e7705ed8e586f4cf532b111e4fccfbe85869
Ibnu Hanafi <62471250+PratnoKEN@users.noreply.github.com>
2023-03-21 23:01:24 +0700

    add icon

--- config.lua
+++ config.lua
@@ -1,21 +1,19 @@
 Config              = {}
 
-Config.DrawDistance = 10.0
-Config.ZoneSize     = {x = 0.3, y = 0.3, z = 0.1}
-Config.MarkerColor  = {r = 100, g = 200, b = 104}
-Config.MarkerType   = 2
-Config.target = false
-
 Config.Locale       = 'en'
 
 Config.Zones = {
-	vector3(-1082.123047, -247.516479, 37.7554371)
+	{
+		pointcoords = vector3(-1082.123047, -247.516479, 37.7554371)
+	}
 }
 
-Config.Blip = {
-	Sprite = 407,
-	Display = 4,
-	Scale = 0.9,
-	Colour = 27,
-	ShortRange = true
+Config.JobIcons = {
+	['unemployed'] = 'fa-solid fa-user',
+	['slaughterer'] = 'fa-solid fa-drumstick-bite',
+	['miner'] = 'fa-solid fa-gem',
+	['fisherman'] = 'fa-solid fa-fish',
+	['tailor'] = 'fa-solid fa-shirt',
+	['fueler'] = 'fa-solid fa-oil-well',
+	['lumberjack'] = 'fa-solid fa-tree',
 }
